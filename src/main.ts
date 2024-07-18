1b2d78c10f229af20de56d7021040278960c55b3
SteveDwumah <Steve-marvin-dwumah@hotmail.de>
2024-07-18 14:18:59 +0200

    Aufgaben

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,89 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+console.log('Welcome :)');
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.log("Halloooo");
 
+let dogName = 'Leon';
+console.log(dogName);
+dogName = (`elf`);
+console.log(dogName);
+
+let catName = 11
+console.log(catName);
+catName = (11)
+console.log(catName);
+
+let seventeen: number = 11
+const sum = seventeen + 44;
+console.log(sum);
+
+// Addition
+let addition_operator: number = 1 + 1;
+console.log('addition: ' + addition_operator);
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log('subtraktion: ' + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log('multiplication: ' + multiplication_operator);
+
+// Division
+let division_operator: number = 4 / 2;
+console.log('division: ' + division_operator);
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log('modulus: ' + modulo_operator);
+
+let x: number = 20
+let y: number = 30
+let z: number = 10
+console.log(x + y);
+console.log(x - y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+let resultOne = x * y / z
+console.log(resultOne);
+
+let a = 15
+let b = 9
+let c = 20
+console.log(a % b);
+let resultTwo = (a + b) * c
+console.log(resultTwo);
+a++
+console.log(a);
+b--
+console.log(b);
+
+let resultThree = a - b
+console.log(resultThree);
+console.log(resultOne % resultTwo);
+
+
+// let score: number = 5 + 5 * 10;  
+// console.log('Ergebnis: ' + score);
+
+// let score = (5 + 5) * 10;  
+// console.log('Ergebnis: ' + score);
+
+let score = 0;  
+score = score + 10;  
+console.log('Ergebnis: ' + score);
+
+score += 10;  
+console.log('Ergebnis: ' + score)
+
+let zahl = 1;  
+zahl = zahl + 1;  
+zahl += 1;
+console.log(zahl);
+
+zahl++;  
+console.log('increment: ' + zahl);
+
+zahl--;  
+console.log('dekrement: ' + zahl);
