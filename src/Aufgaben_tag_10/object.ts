56e38c19ad992bf8e70e990eb55479b01b734355
SteveDwumah <Steve-marvin-dwumah@hotmail.de>
2024-08-01 13:34:45 +0200

    Aufgaben

--- src/Aufgaben_tag_10/object.ts
+++ src/Aufgaben_tag_10/object.ts
@@ -59,4 +59,125 @@ type Apple = {
 
         console.log(unsereHaustiere);
 
+        console.clear()
+
 //!----- Objekte Aufgabe 3 --------!
+
+type Lager = {
+    schreibtisch: {
+        schublade: string;
+    };
+    schrank: {
+        "Obere Schublade": {
+            Ordner1: string;
+            Ordner2: string;
+        };
+        "Untere Schublade": string;
+    };
+};
+
+let unserLager: Lager = {
+    schreibtisch: {
+        schublade: "Hefter"
+    },
+    schrank: {
+        "Obere Schublade": {
+            Ordner1: "Dokumente",
+            Ordner2: "Geheimnisse"
+        },
+        "Untere Schublade": "Cola"
+    }
+};
+
+console.log(unserLager.schrank["Obere Schublade"].Ordner2);
+console.log(unserLager.schrank["Untere Schublade"]);
+console.log(unserLager.schreibtisch.schublade);
+
+console.clear()
+
+//!----- Objekte Aufgabe 4 --------!
+
+type Musik = {
+    kunstler: string;
+    title: string;
+    release_jahr: number;
+    formate: string[];
+    gold: boolean;
+  };
+  
+  let meineTopVier: Musik[] = [
+    {
+      kunstler: 'The Beatles',
+      title: 'Abbey Road',
+      release_jahr: 1969,
+      formate: ['LP', 'CD', 'MC', 'Download'],
+      gold: true
+    },
+    {
+      kunstler: 'Pink Floyd',
+      title: 'Dark Side of the Moon',
+      release_jahr: 1978,
+      formate: ['CS', 'CD', 'LP', 'Download'],
+      gold: true
+    },
+    {
+      kunstler: 'Led Zeppelin',
+      title: 'Led Zeppelin IV',
+      release_jahr: 1971,
+      formate: ['CS', 'LP', 'Download'],
+      gold: true
+    },
+    {
+      kunstler: 'Metallica',
+      title: 'Kill ’Em All und Ride the Lightning',
+      release_jahr: 1983,
+      formate: ['LP', 'CD', 'MC', 'Download'],
+      gold: true
+    }
+  ];
+
+  console.log(meineTopVier[0].kunstler);
+  console.log(meineTopVier[0].formate[3]);
+  console.log(meineTopVier[1].gold);
+  console.log(meineTopVier[2].release_jahr, " und ", meineTopVier[3].release_jahr);
+  console.log(meineTopVier[3].formate[2]);
+  console.log(meineTopVier[3].title.slice(17,21));
+
+  let RedHotChilliPeppers: Musik = {
+    kunstler: 'Steve',
+    title: 'Snow',
+    release_jahr: 2006,
+    formate: ['LP', 'CD', 'MC', 'Download'],
+    gold: true
+  }
+
+  meineTopVier.push(RedHotChilliPeppers)
+
+  console.log(meineTopVier);
+
+  //!----- Objekte Aufgabe 5 --------!
+
+  type PeriodActive = {
+    start: number;
+    end: number | string;
+    extra?: number;
+};
+
+type Singer = {
+    name: string;
+    country: string;
+    period_active: PeriodActive;
+    genre: string;
+};
+
+const singers: Singer[] = [
+    { name: 'The Beatles', country: 'United Kingdom', period_active: { start: 1960, end: 1970 }, genre: "Rock / Pop" },
+    { name: 'Elvis Presley', country: 'United States', period_active: { start: 1954, end: 1977 }, genre: "Rock and roll" },
+    { name: 'Michael Jackson', country: 'United States', period_active: { start: 1964, end: 2009 }, genre: "Pop / Rock / Dance / Soul / R&B" },
+    { name: 'Elton John', country: 'United Kingdom', period_active: { start: 1964, end: "present" }, genre: "Pop / Rock" },
+    { name: 'Madonna', country: 'United States', period_active: { start: 1979, end: "present" }, genre: "Pop / Dance / Electronica" },
+    { name: 'Led Zeppelin', country: 'United Kingdom', period_active: { start: 1968, end: 1980 }, genre: "Hard rock / Blues rock / Folk rock" },
+    { name: 'Rihanna', country: 'United States', period_active: { start: 2005, end: "present" }, genre: "R&B / Pop / Dance / Hip-hop" },
+    { name: 'Pink Floyd', country: 'United Kingdom', period_active: { start: 1965, end: 1996, extra: 2014 }, genre: "Progressive rock / Psychedelic rock" },
+];
+
